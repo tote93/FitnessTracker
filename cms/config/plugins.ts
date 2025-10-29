7dd15fc0a6cda985b5a8d18236bc69d156eb2bd8
tote93 <igorej@hotmail.com>
2025-10-29 22:10:05 +0100

    feat(plugin): Added email support fix(data): Updated datadb

--- cms/config/plugins.ts
+++ cms/config/plugins.ts
@@ -1,14 +1,23 @@
 export default {
-    'schemas-to-ts': {
-        enabled: true,
-        config: {
-            acceptedNodeEnvs: ["development"],
-            commonInterfacesFolderName: 'schemas-to-ts',
-            alwaysAddEnumSuffix: false,
-            alwaysAddComponentSuffix: false,
-            usePrettierIfAvailable: true,
-            logLevel: 2,
-            destinationFolder: undefined,
-        }
+  "schemas-to-ts": {
+    enabled: true,
+    config: {
+      acceptedNodeEnvs: ["development"],
+      commonInterfacesFolderName: "schemas-to-ts",
+      alwaysAddEnumSuffix: false,
+      alwaysAddComponentSuffix: false,
+      usePrettierIfAvailable: true,
+      logLevel: 2,
+      destinationFolder: undefined,
     },
-}
+  },
+  email: {
+    config: {
+      provider: "sendmail",
+      providerOptions: {},
+      settings: {
+        defaultFrom: "email@strapi.mydomain.com",
+      },
+    },
+  },
+};
