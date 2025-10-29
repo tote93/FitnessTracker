7043f770cd0197bf7ad08b60caf71546b165937d
tote93 <igorej@hotmail.com>
2025-10-29 16:32:49 +0100

    chore(strapi): Enable CORS for FE

--- cms/config/middlewares.ts
+++ cms/config/middlewares.ts
@@ -1,5 +1,14 @@
 export default [
   'strapi::logger',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: ['*'],
+      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
+      keepHeaderOnError: true,
+    },
+  },
   'strapi::errors',
   'strapi::security',
   'strapi::cors',
